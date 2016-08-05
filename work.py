b6bf4e5948b65986016a0c0e7ae540d6f4a1513c
??ngel ??lvarez <angel@nan-tic.com>
2016-08-05 11:46:07 +0200

    remove project to use origin

--- work.py
+++ work.py
@@ -1,42 +1,19 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-import datetime
-from decimal import Decimal
-from itertools import izip, chain
-from sql.aggregate import Sum
 
-from trytond.model import Workflow, ModelSQL, ModelView, fields, Unique
-from trytond.pool import Pool, PoolMeta
-from trytond.pyson import Eval, If, Bool
-from trytond.transaction import Transaction
-from trytond.tools import grouped_slice, reduce_ids
-from trytond import backend
+from trytond.model import fields
+from trytond.pool import PoolMeta
 
 __all__ = ['Project', 'ShipmentWork']
+
 
 class ShipmentWork:
     __name__ = 'shipment.work'
     __metaclass__ = PoolMeta
 
-    project = fields.Many2One('project.work', 'Project',
-        domain=[
-            ('party', '=', Eval('party')),
-            ],
-        states={
-            'readonly': Eval('state').in_(['checked', 'cancel']),
-            },
-        depends=['state', 'party'])
-
     @classmethod
-    def __register__(cls, module_name):
-        TableHandler = backend.get('TableHandler')
-        table = TableHandler(cls, module_name)
-
-        # migration from v3.4
-        if table.column_exist('project'):
-            table.column_rename('project', 'work_project')
-
-        super(ShipmentWork, cls).__register__(module_name)
+    def _get_origin(cls):
+        return super(ShipmentWork, cls)._get_origin() + ['project.work']
 
 
 class Project:
@@ -44,7 +21,7 @@ class Project:
     __name__ = 'project.work'
     __metaclass__ = PoolMeta
 
-    shipments = fields.One2Many('shipment.work', 'project', 'Shipment Works')
+    shipments = fields.One2Many('shipment.work', 'origin', 'Shipment Works')
 
     @classmethod
     def _get_cost(cls, works):
